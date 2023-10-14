b37619a298097c04d2c2d8859f557e9d2eed83fa
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-10-14 12:20:47 -0700

    reconciliation sample demo

--- food-villa/src/components/Body.js
+++ food-villa/src/components/Body.js
@@ -4,6 +4,7 @@ import { useState } from "react"
 
 const Body = () => {
     const [searchText, setSearchtext] = useState("")
+    const [sampleBoolean, setSampleBoolean] = useState('true')
     return (
         <div className="body" style={{ display: "flex", flexWrap: "wrap" }}>
             <div className="search-container">
@@ -13,7 +14,14 @@ const Body = () => {
                     placeholder="Search" 
                     value={searchText} 
                     onChange={(e) => setSearchtext(e.target.value)}/>
-                <button className="search-btn">Search</button>
+                <h1>{sampleBoolean}</h1>
+                <button className="search-btn" onClick={() => {
+                    if(sampleBoolean === 'true'){
+                        setSampleBoolean('false')
+                    } else {
+                        setSampleBoolean('true')
+                    }
+                }}>Search</button>
             </div>
             {restaurantList.map((res) => {
                 return <RestaurantCard {...res} key={res.id} />
