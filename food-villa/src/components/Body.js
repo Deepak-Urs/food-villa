b00511df199e76671c008dcbe8df955d3b3bbc2a
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-10-16 19:07:19 -0700

    added useEffect and fetch API call

--- food-villa/src/components/Body.js
+++ food-villa/src/components/Body.js
@@ -1,6 +1,7 @@
 import { restaurantList } from "../constants"
 import RestaurantCard from "./RestaurantCard"
 import { useState } from "react"
+import { swiggy_api_URL } from "../constants"
 
 function filterData (searchText) {
     // taking restaurantList to have original data always
@@ -11,6 +12,23 @@ function filterData (searchText) {
 const Body = () => {
     const [searchText, setSearchtext] = useState("")
     const [restaurants, setRestaurants] = useState(restaurantList)
+
+    const getData = async () => {
+        let res = await fetch(swiggy_api_URL)
+        const jsonData = await res.json()
+        console.log(jsonData?.data?.cards[2].card?.card?.gridElements?.infoWithStyle?.restaurants[0])
+        //.then((res) => {
+        //    let jsonData = res.json()
+        //    console.log('data seen', data)
+        //})
+        //console.log('res seen', res)
+        //setRestaurants(data)
+    }
+
+    useState(() => {
+        getData()
+    }, [restaurantList])
+    
     return (
         <div className="body" style={{ display: "flex", flexWrap: "wrap" }}>
             <div className="search-container">
--- food-villa/src/constants.js
+++ food-villa/src/constants.js
@@ -29,3 +29,9 @@ export const restaurantList = [{
     cuisines: ["Healthy foods", "Mexican"],
     rating: "4.5"
 }]
+
+// Swiggy API to get Restaurant data with corsproxy
+export const swiggy_api_URL =
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
+
+//export const shimmer_card_unit = 20;
