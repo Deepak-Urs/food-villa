01f1f2589a8f77f753c3d185eab0375bc7c7794e
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-10-17 13:12:19 -0700

    updated the data by calling the swiggy API and added conditional rendering

--- food-villa/src/components/Body.js
+++ food-villa/src/components/Body.js
@@ -2,34 +2,43 @@ import { restaurantList } from "../constants"
 import RestaurantCard from "./RestaurantCard"
 import { useState } from "react"
 import { swiggy_api_URL } from "../constants"
+import Shimmer from "./Shimmer"
 
-function filterData (searchText) {
+function filterData (searchText, resData) {
     // taking restaurantList to have original data always
-    return restaurantList.filter((res) => res.name.includes(searchText))
+    let res = resData.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()))
+    console.log('filtered res seen', res);
+    return res;
 }
 
+//function setProcessedRestaurants(data) {
+//    console.log('data seen', ser);
+//})
 
 const Body = () => {
     const [searchText, setSearchtext] = useState("")
-    const [restaurants, setRestaurants] = useState(restaurantList)
+    const [restaurants, setRestaurants] = useState([])
+    const [filteredRestaurants, setFilteredRestaurants] = useState([])
 
     const getData = async () => {
         let res = await fetch(swiggy_api_URL)
         const jsonData = await res.json()
-        console.log(jsonData?.data?.cards[2].card?.card?.gridElements?.infoWithStyle?.restaurants[0])
+        //console.log(jsonData?.data)
+        //console.log(jsonData?.data?.cards[2].card?.card?.gridElements?.infoWithStyle?.restaurants)
         //.then((res) => {
         //    let jsonData = res.json()
         //    console.log('data seen', data)
         //})
         //console.log('res seen', res)
-        //setRestaurants(data)
+        setRestaurants(jsonData?.data?.cards[2].card?.card?.gridElements?.infoWithStyle?.restaurants)
+        setFilteredRestaurants(jsonData?.data?.cards[2].card?.card?.gridElements?.infoWithStyle?.restaurants)
     }
 
     useState(() => {
         getData()
     }, [restaurantList])
     
-    return (
+    return restaurants.length ? (
         <div className="body" style={{ display: "flex", flexWrap: "wrap" }}>
             <div className="search-container">
                 <input
@@ -39,14 +48,15 @@ const Body = () => {
                     value={searchText} 
                     onChange={(e) => setSearchtext(e.target.value)}/>
                 <button className="search-btn" onClick={() => {
-                    setRestaurants(filterData(searchText))
+                    setFilteredRestaurants(filterData(searchText, restaurants))
                 }}>Search</button>
             </div>
-            {restaurants.map((res) => {
-                return <RestaurantCard {...res} key={res.id} />
+            {filteredRestaurants.map((res) => {
+                return <RestaurantCard {...res?.info} key={res?.info?.id} />
             })}
         </div>
-    )
+    ) : 
+    (<Shimmer />)
 }
 
 
--- food-villa/src/components/RestaurantCard.js
+++ food-villa/src/components/RestaurantCard.js
@@ -1,11 +1,11 @@
-const RestaurantCard = ({image, name, cuisines, rating}) => {
-    //const {image, name, cuisines, rating} = props.restaurant
+const RestaurantCard = ({image, name, cuisines, avgRating}) => {
+    //const {image, name, cuisines, avgRating} = props.restaurant
     return (
         <div className="card">
             <img alt="company-logo" src={image} />
             <h2>{name}</h2>
             <h3>{cuisines?.join(", ")}</h3>
-            <h4>{rating} stars</h4>
+            <h4>{avgRating} stars</h4>
         </div>
     )
 }
