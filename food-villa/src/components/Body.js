cf0a42c299464d49aba36be1d1ec264a36108a05
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-10-19 19:12:47 -0700

    fixed minor bug - search

--- food-villa/src/components/Body.js
+++ food-villa/src/components/Body.js
@@ -1,10 +1,10 @@
 import { restaurantList } from "../constants"
 import RestaurantCard from "./RestaurantCard"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { swiggy_api_URL } from "../constants"
 import Shimmer from "./Shimmer"
 
-function filterData (searchText, resData) {
+function filterData(searchText, resData) {
     // taking restaurantList to have original data always
     let res = resData.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()))
     console.log('filtered res seen', res);
@@ -16,47 +16,49 @@ function filterData (searchText, resData) {
 //})
 
 const Body = () => {
+    //avoid rendering component
     const [searchText, setSearchtext] = useState("")
-    const [restaurants, setRestaurants] = useState([])
+    const [allRestaurants, setAllRestaurants] = useState([])
     const [filteredRestaurants, setFilteredRestaurants] = useState([])
 
     const getData = async () => {
         let res = await fetch(swiggy_api_URL)
         const jsonData = await res.json()
-        //console.log(jsonData?.data)
-        //console.log(jsonData?.data?.cards[2].card?.card?.gridElements?.infoWithStyle?.restaurants)
-        //.then((res) => {
-        //    let jsonData = res.json()
-        //    console.log('data seen', data)
-        //})
-        //console.log('res seen', res)
-        setRestaurants(jsonData?.data?.cards[2].card?.card?.gridElements?.infoWithStyle?.restaurants)
+
+        setAllRestaurants(jsonData?.data?.cards[2].card?.card?.gridElements?.infoWithStyle?.restaurants)
         setFilteredRestaurants(jsonData?.data?.cards[2].card?.card?.gridElements?.infoWithStyle?.restaurants)
     }
 
-    useState(() => {
+    useEffect(() => {
         getData()
-    }, [restaurantList])
-    
-    return restaurants.length ? (
+    }, [allRestaurants])
+
+    // early return
+    if(!allRestaurants) return null;
+
+    if(filteredRestaurants.length === 0) {
+        return (<h1>No restaurants matched your filter</h1>)
+    }
+
+    return filteredRestaurants.length === 0 ? (<Shimmer />) :(
         <div className="body" style={{ display: "flex", flexWrap: "wrap" }}>
             <div className="search-container">
                 <input
-                    type="" 
-                    className="search-input" 
-                    placeholder="Search" 
-                    value={searchText} 
-                    onChange={(e) => setSearchtext(e.target.value)}/>
+                    type=""
+                    className="search-input"
+                    placeholder="Search"
+                    value={searchText}
+                    onChange={(e) => setSearchtext(e.target.value)} />
                 <button className="search-btn" onClick={() => {
-                    setFilteredRestaurants(filterData(searchText, restaurants))
+                    setFilteredRestaurants(filterData(searchText, allRestaurants))
                 }}>Search</button>
             </div>
             {filteredRestaurants.map((res) => {
                 return <RestaurantCard {...res?.info} key={res?.info?.id} />
             })}
         </div>
-    ) : 
-    (<Shimmer />)
+    )
+        
 }
 
 
